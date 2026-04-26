e840c7bb9f99daa60cd93234e0d19e443ce8892f
israjali5555-gif <israjali5555@gmail.com>
2026-04-26 16:57:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,11 @@
+const APP_CONFIG = { appName: "ZabTube", version: "1.0.0",
 
+youtube: { apiKey: "YOUR_YOUTUBE_API_KEY_HERE", channelId: "YOUR_CHANNEL_ID_HERE", maxResults: 20 },
+
+googleAuth: { clientId: "YOUR_GOOGLE_CLIENT_ID_HERE", authDomain: "YOUR_PROJECT.firebaseapp.com" },
+
+firebase: { apiKey: "YOUR_FIREBASE_API_KEY", authDomain: "YOUR_PROJECT.firebaseapp.com", projectId: "YOUR_PROJECT_ID", storageBucket: "YOUR_PROJECT.appspot.com", messagingSenderId: "YOUR_SENDER_ID", appId: "YOUR_APP_ID" },
+
+features: { enableLogin: true, enableLibrary: true, enableSubscriptions: true, enableWatchLater: true, enableSearch: true } };
+
+console.log(APP_CONFIG.appName + " config loaded successfully.");
