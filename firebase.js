6237cc5ad26c4968f0a3eea57fc75fda1db45e8a
israjali5555-gif <israjali5555@gmail.com>
2026-04-27 08:19:56 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -11,3 +11,13 @@ function logoutUser() { alert("User logged out successfully."); console.log("Log
 function checkUserSession() { console.log("Checking existing user session..."); }
 
 window.addEventListener("load", function () { initializeFirebase(); checkUserSession(); });
+
+const firebaseConfig = {
+  apiKey: "AiZaSyChWz8PoMxQtgeCWpu...", 
+  authDomain: "zabtube-e81d6.firebaseapp.com",
+  projectId: "zabtube-e81d6",
+  storageBucket: "zabtube-e81d6.firebasestorage.app",
+  messagingSenderId: "1088501191721",
+  appId: "1:1088501191721:web:9f3050ab5c8c6a50a63838",
+  measurementId: "G-G4L1HW8X9Z"
+};
